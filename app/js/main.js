c9e519d0891263acd3b2773e2ec33f26d52538af
Carlos Matheu Armengol <info@plastikaweb.com>
2015-10-21 17:55:11 +0200

    html carousel basic structure

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,29 @@
 /**
  * Created by plastik on 20/10/15.
  */
-'use strict';
-var kk = 'Hi there, welcome!!';
+(function() {
+    'use strict';
 
-console.log(kk);
+    function Carousel(element) {
+
+        this.time = null;
+        this.delay = null;
+        this.currentIndex = null;
+        this.element = element;
+
+        this.default = {
+            time: 500,
+            delay: 3000,
+            currentIndex: 0
+        };
+
+    }
+
+    Carousel.prototype.init = function() {
+        console.log('init');
+    };
+
+    var carousel = new Carousel('#carousel');
+    carousel.init();
+
+}());
--- gruntfile.js
+++ gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            files: {
+            html: {
                 files: ['<%= config.appFolder %>/*.html'],
                 tasks: ['copy:dist']
             },
